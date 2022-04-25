1192a958f4d36d6e641b38c2d4627edccdb292f0
TimmyChan <Timmeh.Chan@gmail.com>
2022-04-24 18:30:12 -0700

    adding index.html

--- submit.sh
+++ submit.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-if [ "$#" -eq 0 ]; then
+message_flag=''
+
+while getopts :m: flag
+do
+    case "${flag}" in
+        m) COMMENT=${OPTARG}; message_flag='true' ;;
+    esac
+done
+
+if  [$message_flag -eq 'true' ]; then
     echo "Enter commit message, followed by [ENTER]: "
     read COMMENT
-else
-	let COMMENT = $1
 fi
 
 rm ./doc/index.html
@@ -12,7 +19,7 @@ jupyter nbconvert euler-problemset-sage.ipynb --to markdown --output README.md
 jupyter nbconvert euler-problemset-sage.ipynb --to slides  --output-dir ./docs/
 
 git add .
-git commit -m "$COMMENT"
+git commit -m "${COMMENT}"
 git push
 
 exit 0
